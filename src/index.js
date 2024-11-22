b7aaec01be71e8ce637b744e580ebe498fffd520
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-22 22:23:21 +0530

    Added basic express router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 
-app.listen(3000, () => {
-    console.log(`Server for Shop-E-Cart is Up`)
-})
+const { PORT } = require("./config/serverConfig");
+
+const { configPingRoutes } = require("./routes/pingRoutes");
+const pingRoutes = require("./routes/pingRoutes");
+
+const app = express();
+app.use("/api/v1/ping", pingRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server for Shopcart is Up ${PORT}`);
+});
---
+++ src/routes/pingRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { pingController } = require("../controllers/pingController");
+
+const router = express.Router();
+
+router.get("/", pingController); // mapping a route to a controller
+
+module.exports = router;
