3c6efdd12521e31e9c26b211ec5cfe74a22c2375
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-24 15:12:03 +0530

    Added basic validation to create product api

--- src/routes/v1/product_router.js
+++ src/routes/v1/product_router.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
+const { createProduct } = require("../../controllers/product_controller");
+const {
+  createProductValidator,
+} = require("../../middlewares/product_middlewares");
+
 const productRouter = express.Router();
 
 productRouter.get("/", (req, res) => {
   return res.json({ products: [] });
 }); // mapping a route to a controller
 
-productRouter.post("/", (req, res) => {
-  return res.json({ products: [] });
-}); // mapping a route to a controller
+productRouter.post("/", createProductValidator, createProduct); // mapping a route to a controller
 
 module.exports = productRouter;
