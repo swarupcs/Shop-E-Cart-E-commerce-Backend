8e67e55bc058e85db954dbaf5356fa74cdae16a6
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-24 15:30:48 +0530

    Added basic in memory db based service layer

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -1,21 +1,45 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
+const ProductService = require("../services/product_service");
+
 function createProduct(req, res) {
   try {
-    // some db processing
+    const response = ProductService.createProduct(req.body);
 
     return res.status(StatusCodes.CREATED).json({
       sucess: true,
       error: {},
       message: ReasonPhrases.CREATED + " Product",
-      data: {
-        id: Math.random() * 20,
-        title: req.body.title,
-        description: req.body.description,
-        category: req.body.category,
-        price: req.body.price,
-        image: req.body.image,
-      },
+      data: response,
+    });
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+}
+
+function getProducts(req, res) {
+  try {
+    const response = ProductService.getProducts();
+    return res.status(StatusCodes.OK).json({
+      sucess: true,
+      error: {},
+      message: "Successfully fetched the Products",
+      data: response,
+    });
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+}
+
+function getProduct(req, res) {
+  // /api/v1/products/2
+  try {
+    const response = ProductService.getProduct(req.params.id);
+    return res.status(StatusCodes.OK).json({
+      sucess: true,
+      error: {},
+      message: "Successfully fetched the Product",
+      data: response,
     });
   } catch (error) {
     console.log("Something went wrong", error);
@@ -24,4 +48,6 @@ function createProduct(req, res) {
 
 module.exports = {
   createProduct,
+  getProducts,
+  getProduct,
 };
