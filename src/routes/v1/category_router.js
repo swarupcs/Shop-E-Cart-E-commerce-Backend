683559869649e92ae2b3b39e80935dd40a6a17c3
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-24 21:21:02 +0530

    Added get category and get all categories api

--- src/routes/v1/category_router.js
+++ src/routes/v1/category_router.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
-const { createCategory } = require("../../controllers/category_controller");
+const {
+  createCategory,
+  getAllCategories,
+  getCategory,
+} = require("../../controllers/category_controller");
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", createCategory); // mapping a route to a controller
+
+
+categoryRouter.get("/", getAllCategories);
+categoryRouter.get("/:id", getCategory);
 
 module.exports = categoryRouter;
--- src/services/category_service.js
+++ src/services/category_service.js
@@ -10,6 +10,16 @@ class CategoryService {
     );
     return response;
   }
+
+  async getAllCategories() {
+    const response = await this.respository.getCategories();
+    return response;
+  }
+
+  async getCategory(categoryId) {
+    const response = await this.respository.getCategory(categoryId);
+    return response;
+  }
 }
 
 module.exports = CategoryService;
