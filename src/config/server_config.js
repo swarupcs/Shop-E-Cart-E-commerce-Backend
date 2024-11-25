8d4f024e54a9187b88782567b7636ab10e2c7188
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-25 22:24:19 +0530

    Added associations

--- src/config/server_config.js
+++ src/config/server_config.js
@@ -8,4 +8,7 @@ module.exports = {
   DB_USER: process.env.DB_USER,
   DB_NAME: process.env.DB_NAME,
   DB_PASS: process.env.DB_PASS,
+
+  DB_ALTER: process.env.DB_ALTER,
+  DB_FORCE: process.env.DB_FORCE,
 };
---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const Category = require("./category");
+const Product = require("./product");
+
+Product.belongsTo(Category, { foreignKey: "categoryId" });
+
+Category.hasMany(Product, { foreignKey: "categoryId" });
+
+module.exports = {
+  Product,
+  Category,
+};
