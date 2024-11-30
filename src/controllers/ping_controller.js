6a82dfb189b98f1f922f8a2146d1ac17c2cf836c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-30 19:19:20 +0530

    Added token verification

--- src/controllers/ping_controller.js
+++ src/controllers/ping_controller.js
@@ -3,6 +3,10 @@ function pingCheck(request, response) {
   return response.json({ message: "OK from V1 API" });
 }
 
+function pingAuthCheck(request, response) {
+  return response.json({ message: "OK" });
+}
+
 function pingCheckV2(request, response) {
   return response.json({ message: "OK from V2 API" });
 }
@@ -10,4 +14,5 @@ function pingCheckV2(request, response) {
 module.exports = {
   pingCheck,
   pingCheckV2,
+  pingAuthCheck,
 };
