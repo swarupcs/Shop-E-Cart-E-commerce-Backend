55a57f54157723fd6817376b9acf4f8eb471ca18
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-30 21:23:48 +0530

    Created cart for user once they signup

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const CategoryController = require("./category_controller");
 const ProductController = require("./product_controller");
 const PingController = require("./ping_controller");
 const UserController = require("./user_controller");
+const CartController = require("./cart_controller");
 
 module.exports = {
   CategoryController,
   ProductController,
   PingController,
   UserController,
+  CartController,
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
 const CategoryService = require("./category_service");
 const ProductService = require("./product_service");
 const UserService = require("./user_service");
+const CartService = require("./cart_service");
 module.exports = {
   CategoryService,
   ProductService,
   UserService,
+  CartService,
 };
